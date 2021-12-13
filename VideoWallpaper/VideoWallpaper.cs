e5d04dc6e2e21e08873137a5343d430c2704b1a7
Mzying2001 <Mzying2001@qq.com>
2021-12-14 00:25:33 +0800

    修复Win11下打开任务视图后退出的bug

--- VideoWallpaper/VideoWallpaper.cs
+++ VideoWallpaper/VideoWallpaper.cs
@@ -15,33 +15,32 @@ namespace VideoWallpaper
             {
                 IntPtr hProgman = DllImports.FindWindow("Progman", null);
                 DllImports.SendMessageTimeout(hProgman, 0x52c, 0, 0, 0, 100, out _);
-                DllImports.EnumWindows(EnumWindowsProc_HideWorkerW, 0);
+
+                IntPtr hWorkerW = default;
+                DllImports.EnumWindows((hwnd, lParam) =>
+                {
+                    IntPtr hDefView = DllImports.FindWindowEx(hwnd, IntPtr.Zero, "SHELLDLL_DefView", null);
+                    if (hDefView == IntPtr.Zero) return true;
+
+                    hWorkerW = DllImports.FindWindowEx(IntPtr.Zero, hwnd, "WorkerW", null);
+                    DllImports.ShowWindow(hWorkerW, 5);
+                    return false;
+                }, 0);
 
                 string videoUrl = null;
                 if (File.Exists(VIDEO_TXT))
                     videoUrl = File.ReadAllText(VIDEO_TXT);
 
                 WallpaperForm wf = new WallpaperForm { VideoUrl = videoUrl };
-                IntPtr hWf = wf.Handle;
-                DllImports.SetParent(hWf, hProgman);
+                DllImports.SetParent(wf.Handle, hWorkerW);
 
+                wf.FormClosing += (s, e) => DllImports.ShowWindow(hWorkerW, 0);
                 wf.ShowDialog();
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-        }
-
-        private static bool EnumWindowsProc_HideWorkerW(IntPtr hwnd, IntPtr lParam)
-        {
-            IntPtr hDefView = DllImports.FindWindowEx(hwnd, IntPtr.Zero, "SHELLDLL_DefView", null);
-            if (hDefView == IntPtr.Zero)
-                return true;
-
-            IntPtr hWorkerW = DllImports.FindWindowEx(IntPtr.Zero, hwnd, "WorkerW", null);
-            DllImports.ShowWindow(hWorkerW, 0);
-            return false;
         }
     }
 }
