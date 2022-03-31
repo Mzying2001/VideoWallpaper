ddab89ef0125488efb05dbbef1ded01b39c292d3
Mzying2001 <Mzying2001@qq.com>
2022-03-31 17:01:20 +0800

    Update VideoWallpaper.cs

--- VideoWallpaper/VideoWallpaper.cs
+++ VideoWallpaper/VideoWallpaper.cs
@@ -22,28 +22,30 @@ namespace VideoWallpaper
                 IntPtr hProgman = DllImports.FindWindow("Progman", null);
                 DllImports.SendMessageTimeout(hProgman, 0x52c, 0, 0, 0, 100, out _);
 
-                IntPtr hWorkerW = default;
+                IntPtr hWorkerW = IntPtr.Zero;
                 DllImports.EnumWindows((hwnd, lParam) =>
                 {
                     IntPtr hDefView = DllImports.FindWindowEx(hwnd, IntPtr.Zero, "SHELLDLL_DefView", null);
-                    if (hDefView == IntPtr.Zero) return true;
-
-                    hWorkerW = DllImports.FindWindowEx(IntPtr.Zero, hwnd, "WorkerW", null);
-                    DllImports.ShowWindow(hWorkerW, 5);
-                    return false;
+                    if (hDefView == IntPtr.Zero)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        hWorkerW = DllImports.FindWindowEx(IntPtr.Zero, hwnd, "WorkerW", null);
+                        DllImports.ShowWindow(hWorkerW, 5);
+                        return false;
+                    }
                 }, 0);
 
-                string videoUrl = null;
-                if (File.Exists(VIDEO_TXT))
-                    videoUrl = File.ReadAllText(VIDEO_TXT);
-
-                WallpaperForm wf = new WallpaperForm { VideoUrl = videoUrl };
+                WallpaperForm wf = new WallpaperForm { VideoUrl = GetSavedVideoUrl() };
                 DllImports.SetParent(wf.Handle, hWorkerW);
 
                 SystemEvents.SessionSwitch += (s, e) =>
                 {
                     //防止屏幕解锁后花屏
-                    if (e.Reason == SessionSwitchReason.SessionUnlock) wf.Reload();
+                    if (e.Reason == SessionSwitchReason.SessionUnlock)
+                        wf.Reload();
                 };
 
                 PowerStatusMonitor psm = new PowerStatusMonitor((context, type, setting) =>
@@ -55,13 +57,10 @@ namespace VideoWallpaper
                 });
                 psm.Register();
 
-                wf.FormClosed += (s, e) =>
-                {
-                    DllImports.ShowWindow(hWorkerW, 0);
-                    psm.UnRegister();
-                };
-
                 Application.Run(wf);
+
+                DllImports.ShowWindow(hWorkerW, 0);
+                psm.UnRegister();
             }
             catch (Exception ex)
             {
@@ -73,6 +72,11 @@ namespace VideoWallpaper
         {
             Process cur = Process.GetCurrentProcess();
             return (from p in Process.GetProcesses() where p.ProcessName == cur.ProcessName && p.Id != cur.Id select p).Any();
+        }
+
+        private static string GetSavedVideoUrl()
+        {
+            return File.Exists(VIDEO_TXT) ? File.ReadAllText(VIDEO_TXT) : null;
         }
     }
 }
