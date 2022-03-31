fd883fc7afe4ef9d02d59f7734f23fdc756a61ee
Mzying2001 <Mzying2001@qq.com>
2022-04-01 00:35:40 +0800

    在资源管理器重启后仍保持动态壁纸

--- VideoWallpaper/VideoWallpaper.cs
+++ VideoWallpaper/VideoWallpaper.cs
@@ -3,20 +3,26 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace VideoWallpaper
 {
     public class VideoWallpaper
     {
-        public const string VIDEO_TXT = "./video.txt";
+        public const string VIDEO_TXT = "./video.txt"; //记录视频路径的文件
+
+        private const string ARG_ONRESTART = "-onRestart"; //重启程序时添加此参数以取消对多开的检测
+
+        private static bool restart = false; //是否要重启程序
 
         [STAThread]
-        public static void Main()
+        public static void Main(string[] args)
         {
             try
             {
-                if (CheckStarted())
+                if (!args.Contains(ARG_ONRESTART) && CheckStarted())
                     throw new Exception("程序已启动");
 
                 IntPtr hProgman = DllImports.FindWindow("Progman", null);
@@ -57,10 +63,21 @@ namespace VideoWallpaper
                 });
                 psm.Register();
 
+                CancellationTokenSource cts = new CancellationTokenSource();
+                Task keepWallpaperTask = KeepWallpaper(hProgman, cts);
+
                 Application.Run(wf);
 
-                DllImports.ShowWindow(hWorkerW, 0);
                 psm.UnRegister();
+                cts.Cancel();
+                if (restart)
+                {
+                    Process.Start(Application.ExecutablePath, ARG_ONRESTART);
+                }
+                else
+                {
+                    DllImports.ShowWindow(hWorkerW, 0);
+                }
             }
             catch (Exception ex)
             {
@@ -77,6 +94,31 @@ namespace VideoWallpaper
         private static string GetSavedVideoUrl()
         {
             return File.Exists(VIDEO_TXT) ? File.ReadAllText(VIDEO_TXT) : null;
+        }
+
+        private static async Task KeepWallpaper(IntPtr hProgman, CancellationTokenSource cts)
+        {
+            // 实现在资源管理器重启后仍保持动态壁纸：
+            // 1、循环检测hProgman是否发生变化
+            // 2、当检测到hProgman变化时重启程序
+
+            await Task.Run(async () =>
+            {
+                while (!cts.IsCancellationRequested)
+                {
+                    IntPtr hProgmanCurrent = DllImports.FindWindow("Progman", null);
+                    if (hProgmanCurrent != IntPtr.Zero && hProgmanCurrent != hProgman)
+                    {
+                        restart = true;
+                        Application.Exit();
+                        break;
+                    }
+                    else
+                    {
+                        await Task.Delay(200);
+                    }
+                }
+            }, cts.Token);
         }
     }
 }
